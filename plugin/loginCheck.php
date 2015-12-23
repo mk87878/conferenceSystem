241f23b57a8ff0ecc71c2132ec08a8cc6a760852
mingwen <mk87878@163.com>
2015-12-23 15:05:56 +0800

    增加普通用户是否登录plugin-loginCheck.php

---
+++ plugin/loginCheck.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: zhangmingwen
+ * Date: 15/12/23
+ * Time: 15:03
+ *
+ * 检查是否已经登录
+ */
+if(empty($_SESSION['userName'])) {
+    echo "<script>alert('Not login or logon timeout.');location.href='../index.html';</script>";//登录超时或者未登录
+    exit;
+}
